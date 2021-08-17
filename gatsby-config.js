7104c4e12f61c923d0ab0e21a86530c05674f12a
Vlad <kiyashko.vlad1@gmail.com>
2021-08-17 23:21:59 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/sections`,
-        name: 'sections',
+        path: `${__dirname}/content/pages`,
+        name: 'cms-pages',
       },
     },
     {
@@ -35,31 +35,18 @@ module.exports = {
         name: 'images',
       },
     },
-    'gatsby-plugin-sharp',
+    'gatsby-plugin-image',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: `gatsby-plugin-netlify-cms-paths`,
             options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
+              // Path to your Netlify CMS config file
+              cmsConfig: `/static/admin/config.yml`,
             },
           },
         ],
