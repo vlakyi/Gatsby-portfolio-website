396df03f29f7bba3151cc725bd0c8580aca0587e
Vlad <kiyashko.vlad1@gmail.com>
2021-08-17 23:25:46 +0200

    Enable color widget, page preview and styling on the admin panel

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,14 @@
 import CMS from 'netlify-cms-app';
+import { ColorControl, ColorPreview } from 'netlify-cms-widget-colorpicker';
 // import uploadcare from 'netlify-cms-media-library-uploadcare'
 // import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import IndexPagePreview from './preview-templates/IndexPagePreview';
+import '../reset.css';
+import PagePreview from './preview-templates/PagePreview';
 
 // CMS.registerMediaLibrary(uploadcare)
 // CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview);
+CMS.registerWidget('color', ColorControl, ColorPreview);
+CMS.registerPreviewTemplate('pages', PagePreview);
+CMS.registerPreviewStyle('../reset.css');
